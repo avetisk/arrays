f58c05c9b3b254b0f2d30b9ce73ff03421839a7b
Avétis KAZARIAN <avetis.k@gmail.com>
2021-10-27 15:38:17 +0200

    fix: wrong expect and explanation for exercise 6

--- src/exercise-6.js
+++ src/exercise-6.js
@@ -1,3 +1,31 @@
-// Same as 5 but use .filter() method.
+// Get movies with a rating of > 4 using .filter() method.
+// e.g fn([
+//   {
+//     id: 70111470,
+//     title: "Die Hard",
+//     boxart: "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+//     uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+//     rating: 4.0,
+//     bookmark: [],
+//   },
+//   {
+//     id: 654356453,
+//     title: "Bad Boys",
+//     boxart: "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+//     uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+//     rating: 5.0,
+//     bookmark: [{ id: 432534, time: 65876586 }],
+//   },
+// ]) ===
+//   [
+//     {
+//       id: 654356453,
+//       title: "Bad Boys",
+//       boxart: "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+//       uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+//       rating: 5.0,
+//       bookmark: [{ id: 432534, time: 65876586 }],
+//     },
+//   ];
 
 module.exports = () => {};
--- tests/exercise-6.test.js
+++ tests/exercise-6.test.js
@@ -22,7 +22,7 @@ const data = [
     title: "The Chamber",
     boxart: "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
     uri: "http://api.netflix.com/catalog/titles/movies/70111470",
-    rating: 4.0,
+    rating: 4.1,
     bookmark: [],
   },
   {
@@ -48,6 +48,14 @@ describe("Exercise 6", () => {
         uri: "http://api.netflix.com/catalog/titles/movies/70111470",
         rating: 5.0,
         bookmark: [{ id: 432534, time: 65876586 }],
+      },
+      {
+        id: 65432445,
+        title: "The Chamber",
+        boxart: "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: 4.1,
+        bookmark: [],
       },
       {
         id: 675465,
